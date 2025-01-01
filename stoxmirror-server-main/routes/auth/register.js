654d1f6c0af915c4046f904659fea6c6075eebd4
AmaraCharles <falsepegasus@gmail.com>
2025-01-01 19:38:47 +0000

    logo removal

--- stoxmirror-server-main/routes/auth/register.js
+++ stoxmirror-server-main/routes/auth/register.js
@@ -1,5 +1,5 @@
 var express = require("express");
-var { hashPassword,sendPasswordOtp,userRegisteration, sendWelcomeEmail,resendWelcomeEmail,resetEmail, sendUserDetails, userRegisteration } = require("../../utils");
+var { hashPassword,sendPasswordOtp,userRegisteration, sendWelcomeEmails,resendWelcomeEmail,resetEmail, sendUserDetails, userRegisteration } = require("../../utils");
 const UsersDatabase = require("../../models/User");
 var router = express.Router();
 const { v4: uuidv4 } = require("uuid");
@@ -97,7 +97,7 @@ router.post("/register", async (req, res) => {
     // Create the new user in the database
     const createdUser = await UsersDatabase.create(newUser);
     const token = uuidv4();
-    sendWelcomeEmail({ to: email, token });
+    sendWelcomeEmails({ to: email, token });
 userRegisteration({firstName,email});
 
     return res.status(200).json({ code: "Ok", data: createdUser });
--- stoxmirror-server-main/utils.js
+++ stoxmirror-server-main/utils.js
@@ -352,7 +352,7 @@ const sendForgotPasswordEmail = async (email) => {
   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 };
 
-const sendWelcomeEmail = async ({ to, token }) => {
+const sendWelcomeEmails = async ({ to, token }) => {
   const nodemailer = require("nodemailer");
   const speakeasy = require("speakeasy");
 
@@ -926,7 +926,7 @@ module.exports = {
   
   sendWithdrawalEmail,
   sendWithdrawalRequestEmail,
-  sendWelcomeEmail,
+  sendWelcomeEmails,
   resendWelcomeEmail,
   resetEmail,
   sendKycAlert,
