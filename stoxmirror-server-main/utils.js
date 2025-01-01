8e6fa5d02d8acd56e16c6bd4fe9ce02ae33418fb
AmaraCharles <falsepegasus@gmail.com>
2025-01-01 19:17:45 +0000

    logo removal

--- stoxmirror-server-main/utils.js
+++ stoxmirror-server-main/utils.js
@@ -452,18 +452,7 @@ const sendWelcomeEmail = async ({ to, token }) => {
       </body>
       </html>
     `,
-    // attachments: [
-    //   {
-    //     filename: 'logo.png', // Replace with your logo filename
-    //     path: './logo.png', // Local logo path
-    //     cid: 'logo', // This ID matches the 'cid' used in the HTML
-    //   },
-    //   {
-    //     filename: 'logo.png', // Replace with your puncture image filename
-    //     path: './logo.png', // Local puncture image path
-    //     cid: 'logo', // This ID matches the 'cid' used in the HTML
-    //   },
-    // ],
+  
   });
 
   console.log("Message sent: %s", info.messageId);
